df8828be5ceede86170efe274390515b619eb5ac
Cameron <camhengland@gmail.com>
2024-08-13 02:35:11 -0600

    edits

--- Diep.io copy test/Assets/Scripts/HealthBar.cs
+++ Diep.io copy test/Assets/Scripts/HealthBar.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+    public CanvasGroup canvasGroup;
+
     public void SetMaxHealth(int health)
     {
         slider.maxValue = health;
@@ -16,10 +18,21 @@ public class HealthBar : MonoBehaviour
     {
         slider.value = health;
     }
+
+    public void ShowHealthBar()
+    {
+        canvasGroup.alpha = 1; // Make the health bar visible.
+    }
+
+    public void HideHealthBar()
+    {
+        canvasGroup.alpha = 0; // Make the health bar invisible.
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        HideHealthBar(); // Hide the health bar by default.
     }
 
     // Update is called once per frame
